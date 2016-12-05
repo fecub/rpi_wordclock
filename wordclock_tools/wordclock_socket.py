2a7369700a82896a0d8ee74bb190eba8ca37d509
Ferit Cubukcuoglu <fcbk67@gmail.com>
2016-12-05 15:14:20 +0100

    socket class added for remote controlling

---
+++ wordclock_tools/wordclock_socket.py
@@ -0,0 +1,30 @@
+import socket
+
+TCP_IP = '192.168.0.132'
+TCP_PORT = 5005
+BUFFER_SIZE = 1024
+
+class wordclock_socket:
+    '''
+    A class for open socket to controll remotely the interface buttons
+    '''
+    def __init__(self):
+        '''
+        setup socket
+        '''
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.bind((TCP_IP, TCP_PORT))
+        self.s.listen(1)
+
+    def open(self):
+        conn, addr = self.s.accept()
+        print 'Connection address:', addr
+        while 1:
+            data = conn.recv(BUFFER_SIZE)
+            print "received data:", data
+            conn.send(data)  # echo
+        # if not data: break
+        # conn.close()
+
+    def close(self):
+        self.s.close()
