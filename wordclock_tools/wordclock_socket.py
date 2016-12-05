44b28f35bd7eee5cb380ef49af35175c84ade880
Ferit Cubukcuoglu <fcbk67@gmail.com>
2016-12-05 16:48:10 +0100

    + new socket connection

--- wordclock_tools/wordclock_socket.py
+++ wordclock_tools/wordclock_socket.py
@@ -1,8 +1,6 @@
 import socket
-
-TCP_IP = '192.168.0.132'
-TCP_PORT = 5005
-BUFFER_SIZE = 1024
+import sys
+from thread import *
 
 class wordclock_socket:
     '''
@@ -12,21 +10,52 @@ class wordclock_socket:
         '''
         setup socket
         '''
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print "Connection initialized"
-        self.s.bind((TCP_IP, TCP_PORT))
-        self.s.listen(1)
-        self.open()
 
-    def open(self):
-        conn, addr = self.s.accept()
-        print 'Connection address:', addr
+        HOST = '192.168.0.132'  # Symbolic name meaning all available interfaces
+        PORT = 8888  # Arbitrary non-privileged port
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print 'Socket created'
+
+        # Bind socket to local host and port
+        try:
+            s.bind((HOST, PORT))
+        except socket.error as msg:
+            print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+            sys.exit()
+
+        print 'Socket bind complete'
+
+        # Start listening on socket
+        s.listen(10)
+        print 'Socket now listening'
+
+        # Function for handling connections. This will be used to create threads
+        def clientthread(conn):
+            # Sending message to connected client
+            conn.send('Welcome to the server. Type something and hit enter\n')  # send only takes string
+
+            # infinite loop so that function do not terminate and thread do not end.
+            while True:
+
+                # Receiving from client
+                data = conn.recv(1024)
+                reply = 'OK...' + data
+                if not data:
+                    break
+
+                conn.sendall(reply)
+
+            # came out of loop
+            conn.close()
+
+        # now keep talking with the client
         while 1:
-            data = conn.recv(BUFFER_SIZE)
-            if not data: break
-            print "received data:", data
-            conn.send(data)  # echo
-        conn.close()
+            # wait to accept a connection - blocking call
+            conn, addr = s.accept()
+            print 'Connected with ' + addr[0] + ':' + str(addr[1])
 
-    def close(self):
-        self.s.close()
+            # start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
+            start_new_thread(clientthread, (conn,))
+
+        s.close()
